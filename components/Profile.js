5bc48f72390b2b2efa1bd05705ea5d54c20553a5
Abhishek <abhipatel8719@gmail.com>
2023-03-18 19:17:44 +0530

    user api doen

--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,30 @@
-import React, { useEffect } from "react";
-import DashboardNav from "./DashboardNav";
-import COLORS from "@/theme/colors";
-import Image from "next/image";
-import styles from "../styles/Profile.module.css";
+import React, { useEffect, useState } from 'react';
+import DashboardNav from './DashboardNav';
+import COLORS from '@/theme/colors';
+import Image from 'next/image';
+import styles from '../styles/Profile.module.css';
+import { parseCookies } from 'nookies';
 
 const Profile = () => {
-
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const cookies = parseCookies();
+      console.log(cookies, 'cokse');
+      try {
+        const jsonResponse = await fetch('http://localhost:3000/api/user', {
+          headers: {
+            authentication: cookies.token,
+          },
+        });
+        const response = await jsonResponse.json();
+        setUser(response.user);
+      } catch (err) {
+        console.log(err, 'err');
+      }
+    };
+    fetchUser();
+  }, []);
   return (
     <>
       <DashboardNav />
@@ -15,10 +34,10 @@ const Profile = () => {
         <div className={`${styles.card}`}>
           <div className={`${styles.upper}`}>
             <Image
-              alt=""
+              alt=''
               priority
-              src="/Profilebg.jpg"
-              className="img-fluid"
+              src='/Profilebg.jpg'
+              className='img-fluid'
               width={300}
               height={70}
             />
@@ -27,36 +46,36 @@ const Profile = () => {
           <div className={`${styles.user} text-center`}>
             <div className={`${styles.profile}`}>
               <Image
-                alt=""
-                src="/Profile.jpg"
-                className="rounded-circle"
+                alt=''
+                src='/Profile.jpg'
+                className='rounded-circle'
                 width={360}
                 height={360}
               />
             </div>
           </div>
 
-          <div className="mt-5 text-center">
-            <h4 className="mb-0">Anant Kumawat</h4>
-            <span className="text-muted d-block mb-2">9876543219</span>
+          <div className='mt-5 text-center'>
+            <h4 className='mb-0'>{user?.name}</h4>
+            <span className='text-muted d-block mb-2'>{user?.email}</span>
 
             <button className={`btn btn-primary btn-sm ${styles.follow}`}>
               Follow
             </button>
 
-            <div className="d-flex justify-content-between align-items-center mt-4 px-4">
+            <div className='d-flex justify-content-between align-items-center mt-4 px-4'>
               <div className={`${styles.stats}`}>
-                <h6 className="mb-0">Followers</h6>
+                <h6 className='mb-0'>Followers</h6>
                 <span>8,797</span>
               </div>
 
-              <div className="stats">
-                <h6 className="mb-0">Projects</h6>
+              <div className='stats'>
+                <h6 className='mb-0'>Projects</h6>
                 <span>142</span>
               </div>
 
-              <div className="stats">
-                <h6 className="mb-0">Ranks</h6>
+              <div className='stats'>
+                <h6 className='mb-0'>Ranks</h6>
                 <span>129</span>
               </div>
             </div>
