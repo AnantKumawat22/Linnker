7ffa99a63b3cb3b0f2cc09f02b61f41a508d4559
Abhishek <abhipatel8719@gmail.com>
2023-03-19 20:35:42 +0530

    verify error

--- pages/api/verifyemail/verify/[id]/[token].js
+++ pages/api/verifyemail/verify/[id]/[token].js
@@ -1,21 +1,22 @@
-import User from "@/models/User";
-import Token from "@/models/Token";
+import User from '@/models/User';
+import Token from '@/models/Token';
 
 export default async function handler(req, res) {
   const { id, token } = req.query;
 
   try {
     let user = await User.findOne({ _id: id });
-    if (!user) return res.send("Invalid link");
+    if (!user) return res.send('Invalid link');
 
     let tokencheck = await Token.findOne({ user: user._id, token });
-    if (!tokencheck) return res.send("Invalid link");
+    if (!tokencheck) return res.send('Invalid link');
 
-    await User.updateOne({ _id: user._id, $set: {verified: true} });
+    await User.updateOne({ _id: user._id }, { $set: { verified: true } });
     await Token.findByIdAndRemove(tokencheck._id);
 
-    res.send("Email verified successfully.");
+    res.send('Email verified successfully.');
   } catch (error) {
-    res.send("An Error Occured");
+    console.log(error, 'error');
+    res.send('An Error Occured');
   }
 }
