be64bb31907f4c2ae61bf5796404d26d43faa20d
Abhishek Patel <abhipatel8719@gmail.com>
2025-01-10 22:53:55 +0530

    fix: update the linker spell

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Linker</title>
+        <title>Linnker</title>
         <meta name="description" content="We make it easy to find best groups for you to join." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
